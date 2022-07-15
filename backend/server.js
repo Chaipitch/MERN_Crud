45df831add00cdc948c6984b8e149d3cac0be3b7
Chaipitch Wongwangpaisarn <chaipitch6@gmail.com>
2022-07-15 16:32:50 +0700

    added more code

--- backend/server.js
+++ backend/server.js
@@ -29,7 +29,13 @@ if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../frontend/build")));
 
   app.get("*", (req, res) =>
-    res.sendFile(__dirname, "../", "frontend", "build", "index.html")
+    res.sendFile(
+      path.resolve(__dirname),
+      "../",
+      "frontend",
+      "build",
+      "index.html"
+    )
   );
 } else {
   app.get("/", (req, res) => {
